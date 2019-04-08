d6c597b330976efdab01b65fb562896f49bd1d80
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2019-04-08 19:13:16 +0200

    workaround dont use!

---
+++ R/insertImageCode2.R
@@ -0,0 +1,73 @@
+saveClipboardImage <- function(fileName, dir = getwd()){
+  library(reticulate)
+  library(rstudioapi)
+
+  filePath <- paste0(dir, "/" , fileName)
+  # Refactoring;2;2;Check how other functions deal with the file saving
+  if(file.exists(filePath)) stop(paste0("File already exists at: ", filePath, ". Did not save the file."))
+
+  tryCatch(import("os"), error = function(e){
+    stop("Can not use python, please configure the reticulate/python setup.")
+  })
+  tryCatch(import("PIL"), error = function(e){
+    stop("Required python package PIL is not installed.")
+  })
+
+  pyCode <- paste0("from PIL import ImageGrab; im = ImageGrab.grabclipboard(); im.save('", filePath, "','PNG')")
+  tryCatch(py_run_string(pyCode), error = function(e){
+    if("AttributeError: 'NoneType' object has no attribute 'save'" == e) stop("Clipboard data is not an image.")
+  })
+}
+
+
+findImgFileName <- function(dirPath, fileType = ".png"){
+  fileNames <- list.files(dirPath)
+  identifierName <- "clipboardImage_"
+
+  idx <- grep(identifierName, fileNames)
+  candidates <- strsplit(fileNames[idx], identifierName)
+  if(!length(candidates)){
+    imgNr <- 1
+  }else{
+    nrs <- sapply(candidates, function(cand){
+      # refactor;2;4;what if other types of names -kopie...
+      as.numeric(strsplit(cand[2], "[.]")[[1]][1])
+    })
+    imgNr <- max(nrs) + 1
+  }
+  return(paste0(identifierName, imgNr, fileType))
+}
+
+
+
+#assumption;2;4; if multiple images are selected and copied they will be added to one
+#open issue;2;3;configure parameter for addins
+#todo;2;3;create an image folder and edit the path accordingly
+insertImageCode <- function(){
+  library(reticulate)
+  library(rstudioapi)
+
+  # oldFileContent <- getActiveDocumentContext()$contents
+  filePath <- getActiveDocumentContext()$path
+  if(!nchar(filePath)) stop("Please save the file before pasting an image.")
+  docId <- getActiveDocumentContext()$id
+  splitted <- strsplit(filePath, "[/]")[[1]]
+  dirPath <- paste(splitted[1:(length(splitted) - 1)], collapse = "/")
+  ImgfileName = findImgFileName(dirPath, fileType = ".png")
+
+  # refactor;3;3;get file ending
+  saveClipboardImage(ImgfileName, dir = dirPath)
+  position <- getActiveDocumentContext()$selection[[1]]$range$start
+  newCode <- paste0("![Plot title. ](", ImgfileName, ")")
+
+  newCode <- paste0("![Plot title. ](", ImgfileName, ")")
+  code <- paste0("\\begin{figure}
+\\begin{center}
+\\fbox{\\includegraphics[width = 0.75\\linewidth]{", ImgfileName, "}}
+\\caption{Caption}
+\\end{center}
+\\end{figure}")
+
+  insertText(position, newCode, id = docId)
+}
+
