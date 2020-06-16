9d59eeb8f6b7ed11928589005bffdd39b25ac460
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2020-06-16 13:54:36 +0200

    Update formatting and documentation

--- R/config.R
+++ R/config.R
@@ -1,81 +1,75 @@
-library(shiny)
-library(miniUI)
-library(shinyjs)
-
-newCode <- 'function(ImgfileName) paste0("\\begin{figure}
+rmd_img_code <- 'function(img_file_name) paste0("\\begin{figure}
 \\begin{center}
-\\fbox{\\includegraphics[width = 0.75 \\linewidth]{", ImgfileName, "}}
+\\fbox{\\include_graphics[width = 0.75 \\linewidth]{", img_file_name, "}}
 \\caption{Caption}
 \\end{center}
 \\end{figure}")'
 
 nwcd <- data.frame(
-  simple = 'function(ImgfileName) paste0("![Plot title. ](", ImgfileName,")")',
-  includegraphics = newCode,
-  stringsAsFactors = FALSE
+  simple = 'function(img_file_name) paste0("![Plot title. ](", img_file_name,")")',
+  include_graphics = rmd_img_code,
+  stringsAsFactors = FALSE # for R < 4.0.0
 )
 
 config <- function() {
 
-  ui <- miniPage(
-    useShinyjs(),
+  ui <- miniUI::miniPage(
+    shinyjs::useShinyjs(),
 
-    gadgetTitleBar("Choose code to insert for graphic:"),
+    miniUI::gadgetTitleBar("Choose code to insert for graphic:"),
 
-    miniContentPanel(
+    miniUI::miniContentPanel(
 
       radioButtons("code", "Code choices - customize in text field:",
                    c("simple" = "simple",
-                     "includegraphics" = "includegraphics")),
+                     "include_graphics" = "include_graphics")),
 
-      textInput("fileName", "file name", "clipboardImage_1.png"),
-      checkboxInput("showField", "Code bearbeiten:", FALSE),
-      uiOutput("editCode"),
+      textInput("file_name", "file name", "clipboardImage_1.png"),
+      checkboxInput("show_field", "Code bearbeiten:", FALSE),
+      uiOutput("edit_code"),
       verbatimTextOutput("code"),
-      h6("Click 'done' to close and then select Addin (or corresponding Keyboard shortcut) to insert image from clipboard to .Rmd.")
+      h6("Click 'done' to close and then select addin (or corresponding Keyboard shortcut) to insert image from clipboard to .Rmd.")
     )
   )
 
   server <- function(input, output, session) {
 
-    global <- reactiveValues(newCode = nwcd)
+    global <- reactiveValues(rmd_img_code = nwcd)
 
-    observeEvent(input$insertCode, {
-      global$newCode[[input$code]] <- input$insertCode
+    observeEvent(input$insert_code, {
+      global$rmd_img_code[[input$code]] <- input$insert_code
     })
 
-    observeEvent(input$showField, {
-      if(!input$showField){
-        shinyjs::hide(id = "editCode")
+    observeEvent(input$show_field, {
+      if(!input$show_field){
+        shinyjs::hide(id = "edit_code")
       }else{
-        shinyjs::show(id = "editCode")
+        shinyjs::show(id = "edit_code")
       }
     })
 
-    finalCode <- reactive({
-      userCode <- gsub("\\", "\\\\", global$newCode[[input$code]], fixed = TRUE)
-      eval(parse(text = paste0("func = ", userCode)))
+    rmd_img_code <- reactive({
+      user_code <- gsub("\\", "\\\\", global$rmd_img_code[[input$code]], fixed = TRUE)
+      eval(parse(text = paste0("func = ", user_code)))
     })
 
     output$code <- renderText({
-      userCode <- gsub("\\", "\\\\", global$newCode[[input$code]], fixed = TRUE)
-      eval(parse(text = paste0("func = ", userCode, ";func('", input$fileName,"')")))
+      user_code <- gsub("\\", "\\\\", global$rmd_img_code[[input$code]], fixed = TRUE)
+      eval(parse(text = paste0("func = ", user_code, ";func('", input$file_name,"')")))
     })
 
-    output$editCode <- renderUI({
+    output$edit_code <- renderUI({
       #refactor;3;3;new variable names
-      textAreaInput("insertCode", "This Code will be inserted:", global$newCode[[input$code]],
+      textAreaInput("insert_code", "This code will be inserted:", global$rmd_img_code[[input$code]],
                     width = "500px", rows = 10)
     })
 
     observeEvent(input$done, {
-      imagecliprEnv$newCode <- finalCode()
+      imageclipr_env$rmd_img_code <- rmd_img_code()
       stopApp()
     })
 
   }
 
-  viewer <- paneViewer(300)
-
-  runGadget(ui, server, viewer = viewer)
+  runGadget(ui, server, viewer = paneViewer(300))
 }
--- R/insertImageCode.R
+++ R/insertImageCode.R
@@ -1,109 +1,135 @@
-imagecliprEnv <- new.env(parent = emptyenv())
-imagecliprEnv$newCode <- function(ImgfileName) paste0("![Plot title. ](", ImgfileName, ")")
+imageclipr_env <- new.env(parent = emptyenv())
+imageclipr_env$newCode <- function(img_file_name) paste0("![Plot title. ](", img_file_name, ")")
 
-grabclipboard <- function(filepath){
+#' Saving the clipboard image to disk
+#'
+#'The image from the clipboard has to be saved to disk. Mac, Linux and Windows are supported. For Windows Powershell is used. For Linux xclip is required.
+#'
+#' @param file_name The image from the clipboard has to be saved to disk. file_name is the name of the image when saving it to disk.
+#' @param dir The image from the clipboard has to be saved to disk. dir is the directory to save the image in. By default it is the current working directory.
+#'
+save_clipboard_image <- function(file_name, dir = getwd()) {
+
+  file_path <- file.path(dir, file_name)
+  if (file.exists(file_path)){
+    stop(paste0("File already exists at: ", file_path, ". Did not save the file."))
+  }
+
   platform <- Sys.info()[1]
-  if (platform == "Darwin") {
+
+  if (platform == "Darwin") { # MAC OS
+
     script <- paste0(
       "osascript -e \'
-      set theFile to (open for access POSIX file \"", filepath, "\" with write permission)
+      set theFile to (open for access POSIX file \"", file_path, "\" with write permission)
       try
       write (the clipboard as «class PNGf») to theFile
       end try
       close access theFile'"
-      )
+    )
     system(script)
 
-  } else if (platform == 'Windows') {
+  } else if (platform == "Windows") {
+
     script <- paste0(
       "powershell -sta \"\n",
       "  Add-Type -AssemblyName System.Windows.Forms;\n",
       "  if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {\n",
       "    [System.Drawing.Bitmap][System.Windows.Forms.Clipboard]::GetDataObject().getimage().Save('",
-      paste0(filepath, "', [System.Drawing.Imaging.ImageFormat]::Png) \n"),
+      paste0(file_path, "', [System.Drawing.Imaging.ImageFormat]::Png) \n"),
       "  }\""
     )
     system(script)
-  }else if (platform == 'Linux') {
-    tryCatch(targets <- tolower(system("xclip -selection clipboard -t TARGETS -o",intern=T)), error = function(e){
-      stop("Please install the required system dependency xclip")
-    }) # Validate xclip is installed and get targets from clipboard
-    if (any(grepl(".*png$",targets))){
-      system(paste0("xclip -selection clipboard -t image/png -o > ", filepath))
+
+  } else if (platform == "Linux") {
+
+    # Validate xclip is installed and get targets from clipboard
+    tryCatch(targets <- tolower(system("xclip -selection clipboard -t TARGETS -o", intern = T)), error = function(e) {
+      stop(
+        "Getting image from clipboard with xclip failed. The used command is: 'xclip -selection clipboard -t TARGETS -o'.
+         Please ensure that the required system dependency xclip is installed and can be used. Otherwise file an issue at Github."
+      )
+    })
+
+    if (any(grepl(".*png$", targets))) {
+      system(paste0("xclip -selection clipboard -t image/png -o > ", file_path))
     }
+
   }
 
-  # writeLines(script)
-
-  # in mac os, if no image in clipboard, exec script will create a empty image
-  # in window, no image be create
-  if(file.exists(filepath) &&  file.size(filepath) > 0) {
-    return(filepath)
-  }
-  NULL
-}
-
-saveClipboardImage <- function(fileName, dir = getwd()){
-  filePath <- file.path(dir, fileName)
-  # Refactoring;2;2;Check how other functions deal with the file saving
-  if(file.exists(filePath)) stop(paste0("File already exists at: ", filePath, ". Did not save the file."))
-
-  filePath <- grabclipboard(filePath)
-  if (is.null(filePath)) {
+  # check if a valid picture was saved.
+  # In mac os, if no image is in the clipboard, exec script will create a empty image
+  # In windows, no image will be created
+  if (file.exists(file_path) && file.size(file_path) > 0) {
     stop("Clipboard data is not an image.")
   }
+
+
 }
 
 
-findImgFileName <- function(filePath, fileType = ".png"){
-  # this is to check whether there are identical names
+#' Create a valid, unique file name.
+#'
+#' In case the current file name, for the image, is already taken an index will be added to the file name.
+#'
+#' @param file_path Directory path including name of the file
+#' @param file_type Extension of the image, e.g. .png
+#'
+#' @return An unique name for the image for the given path
+create_valid_file_name <- function(file_path, file_type = ".png") {
 
-  dirPath <- dirname(filePath)
-  identifierName <- paste0( gsub(paste0("\\.", tools::file_ext(filePath)), "", basename(filePath)), "_insertimage_" )
+  dir_path <- dirname(file_path)
+  file_name_id <- paste0(gsub(paste0("\\.", tools::file_ext(file_path)), "", basename(file_path)), "_insertimage_")
 
-  fileNames <- list.files(dirPath)
+  file_names <- list.files(dir_path)
 
-  idx <- grep(identifierName, fileNames)
-  candidates <- strsplit(fileNames[idx], identifierName)
-  if(!length(candidates)){
-    imgNr <- 1
-  }else{
-    nrs <- sapply(candidates, function(cand){
+  idx <- grep(file_name_id, file_names)
+  candidates <- strsplit(file_names[idx], file_name_id)
+
+  if (!length(candidates)) {
+    img_nr <- 1
+  } else {
+    nrs <- sapply(candidates, function(cand) {
       # refactor;2;4;what if other types of names -kopie...
       as.numeric(strsplit(cand[2], "[.]")[[1]][1])
     })
-    imgNr <- max(nrs) + 1
+    img_nr <- max(nrs) + 1
   }
-  return(paste0(identifierName, imgNr, fileType))
+  return(paste0(file_name_id, img_nr, file_type))
 }
 
 
+# open issue;2;3;configure parameter for addins
+# todo;2;3;create an image folder and edit the path accordingly
 
-#assumption;2;4; if multiple images are selected and copied they will be added to one
-#open issue;2;3;configure parameter for addins
-#todo;2;3;create an image folder and edit the path accordingly
-insertImageCode <- function(){
-  func = imagecliprEnv$newCode
-  #print(func)
-  library(reticulate)
-  library(rstudioapi)
+#' Insert the markdown code for the image in the .Rmd file.
+#'
+#' Given the image in the clipboard, the corresponding markdown code will be inserted in the .Rmd file. The code
+#' will be generated given the code template from the configuration and the generated file path. It will be inserted with
+#' the rstudioapi package.
+#' Unsaved documents as Untitled1.Rmd can not properly be accessed by rstudioapi. The document has to be saved to disk.
+#' If multiple images are copied only one will be copied.
+#' The image can only be inserted in the source editor not in the console nor the terminal.
+#'
+insert_image_code <- function() {
 
-  # if(Sys.info()['sysname'] == "Linux") stop("The addin only supports MacOS and Windows.")
+  doc_id <- rstudioapi::getActiveDocumentContext()$id
+  if (doc_id %in% c("#console", "#terminal")){
+    stop("You can`t insert an image in the console nor in the terminal. Please select a line in the source editor.")
+  }
 
-  # oldFileContent <- getActiveDocumentContext()$contents
-  docId <- getActiveDocumentContext()$id
-  if(docId %in% c("#console", "#terminal")) stop("You can`t insert an image in the console nor in the terminal.
-                                                 Please select a line in the source editor.")
-  filePath <- getActiveDocumentContext()$path
-  if(!nchar(filePath)) stop("Please save the file before pasting an image.")
+  file_path <- rstudioapi::getActiveDocumentContext()$path
+  if (!nchar(file_path)){
+    stop("Please save the file before pasting an image.")
+  }
 
   # if the first is tilde, then the python code breaks. Let's replace this using Sys.getenv
-  filePath <- gsub( "^~", Sys.getenv("HOME"), filePath)
-
-  ImgfileName = findImgFileName(filePath, fileType = ".png")
+  file_path <- gsub("^~", Sys.getenv("HOME"), file_path)
+  img_file_name <- create_valid_file_name(file_path, file_type = ".png")
 
   # refactor;3;3;get file ending
-  saveClipboardImage(ImgfileName, dir = dirname(filePath))
-  position <- getActiveDocumentContext()$selection[[1]]$range$start
-  insertText(position, func(ImgfileName), id = docId)
+  save_clipboard_image(img_file_name, dir = dirname(file_path))
+  position <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start
+  code_to_insert <- imageclipr_env$newCode
+  rstudioapi::insertText(position, code_to_insert(img_file_name), id = doc_id)
 }
